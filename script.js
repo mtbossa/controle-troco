f753b27e0aa211ffd5665184c1493bc8a20c4221
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-06-07 19:12:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const values = [
+    [0.05, '0,05', 'cincoCentavos'], 
+    [0.10, '0,10', 'dezCentavos'],
+    [0.25, '0,25', 'vinceCincoCentavos'],
+    [0.50, '0,50', 'cinquentaCentavos'],
+    [1.00, '1,00', 'umReal'],
+    [2.00, '2,00', 'doisReais'],
+    [5.00, '5,00', 'cincoReais'],
+    [10.00, '10,00', 'dezReais'],
+    [20.00, '20,00', 'vinteReais'],
+    [50.00, '50,00', 'cinquentaReais'],
+    [100.00, '100,00', 'cemReais'],
+    [200.00, '200,00', 'duzentosReais']
+];
+
+const flexContainer = document.querySelector('#flex-container');
+
+values.forEach(valorAtual => {
+
+    const moneyColumn = document.createElement('div');
+    moneyColumn.classList.add('money-column');
+
+    const moneyValueH2 = document.createElement('h2');
+    moneyValueH2.innerHTML = `R$ ${valorAtual[1]}`;
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `${valorAtual[2]}`);
+    label.innerHTML = '<h4>Quantidade</h4>'
+
+    const input = document.createElement('input');
+    input.setAttribute('name', `${valorAtual[2]}`);
+    input.setAttribute('id', `${valorAtual[2]}`);
+    input.setAttribute('type', 'number');
+    input.setAttribute('min', '0');
+    input.setAttribute('max', '10000');
+
+    const valorTotalH3 = document.createElement('h3');
+    valorTotalH3.innerHTML = 'Valor Total:';
+
+    const valorMonetario = document.createElement('h2');
+    valorMonetario.classList.add('valor-monetario'); 
+    valorMonetario.innerHTML = 'R$ 0,00';
+
+    moneyColumn.appendChild(moneyValueH2);
+    moneyColumn.appendChild(label);
+    moneyColumn.appendChild(input);
+    moneyColumn.appendChild(valorTotalH3);
+    moneyColumn.appendChild(valorMonetario);
+
+    flexContainer.appendChild(moneyColumn);
+    
+});
+
+
+
+
